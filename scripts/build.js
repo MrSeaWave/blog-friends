02dffe0a6a75f4bac17a92101e4949cafc9e2980
MrSeaWave <774866545@qq.com>
2021-01-13 16:48:27 +0800

    feat: add hexo’s blog

--- scripts/build.js
+++ scripts/build.js
@@ -2,13 +2,24 @@ const yaml = require("js-yaml");
 const { sync: mkdirp } = require("mkdirp"); // 跨平台创建包
 const { writeFileSync, readFileSync } = require("fs");
 const { join: pathJoin } = require("path");
+const child_process = require("child_process");
+
+function copyDir(src, dist) {
+  child_process.spawnSync("cp", ["-r", src, dist]);
+}
 
 try {
   const dir = pathJoin(__dirname, "../dist");
-  const doc = yaml.load(readFileSync(pathJoin(__dirname,'../src/links.yml'), "utf8"));
+  const doc = yaml.load(
+    readFileSync(pathJoin(__dirname, "../src/links.yml"), "utf8")
+  );
+  // 创建dist
   mkdirp(dir);
+  // 复制json至dist文件夹
   const filePath = pathJoin(dir, "./links.json");
   writeFileSync(filePath, JSON.stringify(doc));
+  // 复制图片至dist文件夹
+  copyDir(pathJoin(__dirname, "../src/images"), pathJoin(dir, "./images"));
 } catch (e) {
   console.log("error：", e);
 }
