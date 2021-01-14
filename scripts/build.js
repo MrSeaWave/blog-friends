8088bce2184ab50ec71c15640ae2bd6f24f03251
MrSeaWave <774866545@qq.com>
2021-01-14 10:41:08 +0800

    feat: add html

--- scripts/build.js
+++ scripts/build.js
@@ -4,8 +4,8 @@ const { writeFileSync, readFileSync } = require("fs");
 const { join: pathJoin } = require("path");
 const child_process = require("child_process");
 
-function copyDir(src, dist) {
-  child_process.spawnSync("cp", ["-r", src, dist]);
+function copyDir(from, to) {
+  child_process.spawnSync("cp", ["-r", from, to]);
 }
 
 try {
@@ -20,6 +20,11 @@ try {
   writeFileSync(filePath, JSON.stringify(doc));
   // 复制图片至dist文件夹
   copyDir(pathJoin(__dirname, "../src/images"), pathJoin(dir, "./images"));
+  // 复制html
+  copyDir(
+    pathJoin(__dirname, "../src/index.html"),
+    pathJoin(dir, "./index.html")
+  );
 } catch (e) {
   console.log("error：", e);
 }
